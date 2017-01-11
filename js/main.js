89ba4ff264c61505fcb92fc92875fe86be5aa3e5
Blother <ksjones@udel.edu>
2017-01-12 09:38:14 +1300

    Combined first mechanics of our game

--- js/main.js
+++ js/main.js
@@ -7,11 +7,14 @@ MonsterMystery = {
 
 function preload() {
 
-    game.load.spritesheet('spinner', 'assets/sprites/bluemetal_32x32x4.png', 32, 32);
     game.load.image('phaser', 'assets/sprites/phaser2.png');
     game.load.script('filterX', 'js/lib/filters/BlurX.js');
     game.load.script('filterY', 'js/lib/filters/BlurY.js');
     game.load.script('gray', 'js/lib/filters/Gray.js');
+
+    game.load.image('background','assets/tests/debug-grid-1920x1920.png');
+    game.load.image('npc','assets/sprites/sonic_havok_sanity.png')
+    game.load.image('player','assets/sprites/phaser-dude.png');
 
 }
 
@@ -20,6 +23,10 @@ var sprites;
 var cursors;
 var testImage;
 
+var player;
+var npc;
+var cursors;
+
 
 
 function create() {
@@ -27,6 +34,23 @@ function create() {
 
     // var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'phaser');
     // logo.anchor.setTo(0.5, 0.5);
+
+    game.add.tileSprite(0, 0, 1920, 1920, 'background');
+    game.world.setBounds(0, 0, 1920, 1920);
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+    npc = game.add.sprite(game.world.centerX/2, game.world.centerY/2, 'npc');
+
+    //game.physics.p2.enable(player);
+    game.physics.enable([player,npc], Phaser.Physics.ARCADE);
+    npc.body.immovable = true;
+    player.fixedRotation = true;
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    game.camera.follow(player);
 
     var blurX = game.add.filter('BlurX');
     var blurY = game.add.filter('BlurY');
@@ -39,10 +63,8 @@ function create() {
     //  Here we create a group, populate it with sprites, give them all a random velocity
     //  and then check the group against itself for collision
 
-    sprites = game.add.physicsGroup(Phaser.Physics.ARCADE);
 
-
-    testImage = new MonsterMystery.GameImage(game, 'phaser', game.world.centerX, game.world.centerY, gray);
+    testImage = new MonsterMystery.GameImage(game, 'phaser', game.world.centerX/2 + 300, game.world.centerY/2, gray);
     // testImage.toggle();
     // console.log(testImage);
     // testImage.toggle();
@@ -68,7 +90,46 @@ function create() {
 
 
 function update(){
-    game.physics.arcade.collide(sprites);
+    //player.body.setZeroVelocity();
+    player.body.velocity.setTo(0, 0);
+    player.body.angularVelocity = 0;
 
+    game.physics.arcade.collide(player, npc, collisionHandler, null, this);
+    
+
+    if (cursors.up.isDown)
+    {
+        //player.body.moveUp(300)
+        player.body.velocity.y = -300;
+    }
+    else if (cursors.down.isDown)
+    {
+        //player.body.moveDown(300);
+        player.body.velocity.y = 300;
+    }
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -300;
+    }
+    else if (cursors.right.isDown)
+    {
+        //player.body.moveRight(300);
+        player.body.velocity.x = 300;
+    }
+
+
+}
+
+function collisionHandler (obj1, obj2) {
+    console.log("collision handler!");
+    player.tint = 0xdd0c39;
+    npc.tint = 0xdd0c39;
+}
+
+function render() {
+
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(player, 32, 500);
 
 }
