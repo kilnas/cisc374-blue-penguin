e12a2ec3318746b21440eaf18b3104a860a9324f
Blother <ksjones@udel.edu>
2017-01-11 22:42:24 +1300

    Toggle filter on sprite click working

--- js/image.js
+++ js/image.js
@@ -7,7 +7,9 @@ MonsterMystery.GameImage = function(game, asset, x, y, filter){
   this.filter = filter;
 
   this.inputEnabled = true;
-  this.events.onInputDown.add(this.toggle);
+  this.events.onInputDown.add(this.toggle, this);
+  this.toggle();
+  this.toggle();
 
   game.add.existing(this);
 
@@ -19,12 +21,12 @@ MonsterMystery.GameImage.prototype = Object.create(Phaser.Sprite.prototype);
 MonsterMystery.GameImage.prototype.constructor = MonsterMystery.GameImage;
 
 MonsterMystery.GameImage.prototype.update = function() {
-  if (this.filtered) {
-    this.filters = [ ];
-  }
-  else {
-    this.filters = [ this.filter ];
-  }
+  // if (this.filtered) {
+  //   this.filters = [ ];
+  // }
+  // else {
+  //   this.filters = [ this.filter ];
+  // }
 }
 
 //This is the function to toggle the filter that is on the image
@@ -33,12 +35,13 @@ MonsterMystery.GameImage.prototype.toggle = function(){
   if(this.filtered){
     console.log("none");
     this.filtered = false;
-    // this.filters = [ ];
+    this.filters = null;
   }
   else{
     console.log("gray");
     this.filtered = true;
-    // this.filters = [ this.filter ];
+    this.filters = [ this.filter ];
+    console.log(this.filters);
   }
 
 }
