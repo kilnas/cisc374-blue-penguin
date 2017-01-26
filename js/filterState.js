d312982211ce8bded33ee969ae7d97c5c74e714e
kilnas <kalina@udel.edu>
2017-01-26 07:01:56 -0500

    setup undefined error fix

--- js/filterState.js
+++ js/filterState.js
@@ -4,7 +4,7 @@ var imageState = {
     //format for shader list: [[shader, textName, passes]
     //note: passes removed for now bc not working
     /*var puzzle = new filterClass(game, 'turtlePic1', [[blurShader, "BLUR",0], [blurShader,"COOLBLUR",1], [blurShader, "TUMBLUR",2]]);*/
-    //var puzzle = new filterClass(game, 'turtlePic1', [[blurShader, "BLUR", 1]])
+    var puzzle = new filterClass(game, 'turtlePic1', [[blurShader, "BLUR", 1]])
     puzzle.setup();
 
   },
